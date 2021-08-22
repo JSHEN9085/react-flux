0164e9e6d54e0368d89a50013192c14587dcac9c
JSHEN9085 <jshen9085@gmail.com>
2021-08-22 13:59:36 -0400

    added store

---
+++ src/stores/courseStore.js
@@ -0,0 +1,12 @@
+import {EventEmitter} from 'events'; 
+
+class courseStore extends EventEmitter {
+    addChangeListener(callback) {
+        this.on("change", callback); 
+    };
+
+    removeChangeListener(callback) {
+        this.removeListener("change", callback); 
+    }; 
+
+}
